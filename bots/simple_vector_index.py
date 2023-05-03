acca87a4d82405696a5844b8d76aa8603d752318
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2023-05-03 13:41:55 +0200

    Add files via upload

---
+++ bots/simple_vector_index.py
@@ -0,0 +1,60 @@
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, ServiceContext, Document
+
+
+def load_knowledge() -> list[Document]:
+    # Load data from directory
+    documents = SimpleDirectoryReader('knowledge').load_data()
+    return documents
+
+
+def create_index() -> GPTSimpleVectorIndex:
+    print('Creating new index')
+    # Load data
+    documents = load_knowledge()
+    # Create index from documents
+    service_context = ServiceContext.from_defaults(chunk_size_limit=3000)
+    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+    save_index(index)
+    return index
+
+
+def save_index(index: GPTSimpleVectorIndex):
+    # Save index to file
+    index.save_to_disk('knowledge/index.json')
+
+
+def load_index() -> GPTSimpleVectorIndex:
+    # Load index from file
+    try:
+        index = GPTSimpleVectorIndex.load_from_disk('knowledge/index.json')
+    except FileNotFoundError:
+        index = create_index()
+    return index
+
+
+def query_index(index: GPTSimpleVectorIndex):
+    # Query index
+    while True:
+        prompt = input("Type prompt...")
+        response = index.query(prompt)
+        print(response)
+
+
+def main():
+    # Ask user if they want to refresh the index
+    refresh_index = input("Do you want to refresh the index? (y/n) [n]: ")
+    refresh_index = refresh_index.lower() == 'y'
+
+    # If refreshing the index, create new index and save to file
+    if refresh_index:
+        index = create_index()
+    # Otherwise, load index from file
+    else:
+        index = load_index()
+
+    # Query index
+    query_index(index)
+
+
+if __name__ == '__main__':
+    main()
